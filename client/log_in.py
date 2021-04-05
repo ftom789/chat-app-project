9e341ecc5a253e3539661132c95a4e3a3b095847
ofekn2004 <81981316+ofekn2004@users.noreply.github.com>
2021-04-05 21:36:02 +0300

    log in sign up

---
+++ client/log_in.py
@@ -0,0 +1,30 @@
+from gui import App
+import tkinter
+
+app=App([])
+app.CreateWindow()
+loging=app.CreateFrame()
+loging.pack()
+user_label=app.CreateLabel(loging,"Enter username")
+pass_label=app.CreateLabel(loging,"Enter password")
+user_label.grid(row=1,column=0)
+pass_label.grid(row=2,column=0)
+stringvar_user=app.CreateStringVar()
+stringvar_pass=app.CreateStringVar()
+user_entry= app.CreateEntry(loging,stringvar_user)
+user_entry.grid(row=1,column=1)  
+pass_entry= app.CreateEntry(loging,stringvar_pass)
+pass_entry.grid(row=2,column=1)  
+login_butten=app.CreateButton(loging, text="Log in", bg= "blue")
+signup_butten=app.CreateButton(loging, text= "sign up", bg= "yellow")
+login_butten.grid(row=3,column=0)
+signup_butten.grid(row=3,column=1)
+
+
+
+
+
+
+
+app.mainloop()
+
